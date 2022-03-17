2338db8a8ddd06a32b5d00a660b191eb9857f4b6
Jciborski <kubaciborski@interia.pl>
2022-03-17 14:09:43 +0100

    #3 formatowanie cen ulepszen/osobny plik  na formatowanie

--- src/Scripts/modules/upgrades.js
+++ src/Scripts/modules/upgrades.js
@@ -1,33 +1,25 @@
- const menelUpgradeButton = document.getElementById('upgrade1')
+const menelUpgradeButton = document.getElementById("upgrade1");
 
- 
- 
- let upgradeLevel = 0;
- let upgradeCost = 10;
- let upgradePower = 0
- 
+let upgradeLevel = 0;
+let upgradeCost = 10;
+let upgradePower = 0;
+let upgradeCostFormat = upgradeCost
 
 
+function menelUpgrade() {
+  if (counter >= upgradeCost) {
+    counter = counter - upgradeCost;
 
+    returnedFormatedValue = format(counter);
 
+    upgradeCost = upgradeCost + 10;
+    upgradeLevel = upgradeLevel + 1;
+    upgradePower = upgradePower + 1;
+    upgradeCostFormat = format(upgradeCost)
 
-function menelUpgrade()
-{
-     if(counter>=upgradeCost)
-    {  
-       
-        counter = counter - upgradeCost; 
-        upgradeCost = upgradeCost + 10;
-        upgradeLevel = upgradeLevel + 1;
-        upgradePower = upgradePower + 1;
-        
-        document.getElementById('counter').innerHTML = counter + ' $';
-        document.getElementById('menellevel').innerHTML = upgradeLevel;
-        document.getElementById('menelcost').innerHTML = upgradeCost +' $';
-        
-
-    }
-
+    document.getElementById("counter").innerHTML = returnedFormatedValue + " $";
+    document.getElementById("menellevel").innerHTML = upgradeLevel;
+    document.getElementById("menelcost").innerHTML = upgradeCostFormat + " $";
+  }
 }
-menelUpgradeButton.onclick= menelUpgrade;
-
+menelUpgradeButton.onclick = menelUpgrade;
