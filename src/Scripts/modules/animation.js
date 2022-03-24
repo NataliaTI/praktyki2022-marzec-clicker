f41a19441e7c10376c439e8b5ec025a60a3f0223
marcinfras <marcinfras2003@gmail.com>
2022-03-24 11:06:32 +0100

    #9 skończona animacja klikniec

--- src/Scripts/modules/animation.js
+++ src/Scripts/modules/animation.js
@@ -1,5 +1,6 @@
 const clicker = document.querySelector('.clicker-img');
 
+extraMoneyPerClick = 1;
 
     clicker.addEventListener('click', function(e) {
         let x = e.clientX - e.target.offsetLeft;
@@ -16,8 +17,25 @@ const clicker = document.querySelector('.clicker-img');
         ripples.appendChild(beer);
         
 
+        let money = document.createElement('div');
+        money.classList.add('click');
+        money.style.left = x + 'px';
+        money.style.top = y + 'px';
+        this.appendChild(money);
+
+         let moneyClick = document.createElement('span');
+        moneyClick.classList.add('moneyClick');
+        money.appendChild(moneyClick);
+
+        moneyClick.textContent = '+' + extraMoneyPerClick;
+
+        setTimeout(() => {
+          money.remove()
+         },1000);
+
         setTimeout(() => {
             ripples.remove()
-        },1000);
+        },1500);
+
 
     })
