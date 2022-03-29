46c877d2768bb1fb9fffa11a3dba253365960543
WojtekAndrejczuk03 <andrejczuk.wojtek@gmail.com>
2022-03-29 14:21:14 +0200

    Połączenie ulepszeń w jedną funkcję

--- Layout/main.js
+++ Layout/main.js
@@ -1,95 +1,39 @@
 import { menuUpgrades } from '../src/Scripts/modules/menu.js';
 import { changeCounterElementText, onClickHandler } from '../src/Scripts/modules/onClickIncrement.js';
-import { otwieraczUpgrade, mietekUpgrade, sebaUpgrade, grazynaUpgrade, gangUpgrade, monopolowyUpgrade, browarUpgrade, destylarniaUpgrade, timer } from "../src/Scripts/modules/upgrades.js";
+import { updateList, timer, upgrade } from "../src/Scripts/modules/upgrades.js";
 import {} from "./animation";
 
 // guzik do klikania
 const counterButtonElement = document.getElementById("counter-button");
 
 // ulepszenia
-const otwieraczUpgradeButton = document.getElementById("upgrade1");
-const mietekUpgradeButton = document.getElementById("upgrade2");
-const sebaUpgradeButton = document.getElementById("upgrade3");
-const grazynaUpgradeButton = document.getElementById("upgrade4");
-const gangUpgradeButton = document.getElementById("upgrade5");
-const monopolowyUpgradeButton = document.getElementById("upgrade6");
-const browarUpgradeButton = document.getElementById("upgrade7");
-const destylarniaUpgradeButton = document.getElementById("upgrade8");
+const upgradeFromHtml = document.getElementsByClassName("menu__upgrades-list-item");
 
 export let counter = 15000000;
 export let autoClick = 0;
+export let extraMoneyPerClick = 0;
+
+if (upgradeFromHtml.length) {
+    for (let name = 0; name < upgradeFromHtml.length; name++) {
+        const upgradeDiv = upgradeFromHtml[name];
+        let upgradeId = upgradeDiv;
+        upgradeId.id;
+
+        upgradeDiv.addEventListener('click', (event) => {
+            const result = upgrade(counter, autoClick, extraMoneyPerClick, upgradeId.id);
+            
+            counter = result.counter;
+            autoClick = result.autoClick;
+            extraMoneyPerClick = result.extraMoneyPerClick;
+        });
+    }
+}
 
 if (counterButtonElement) {
     counterButtonElement.addEventListener('click', (event) => {
         counter = onClickHandler(counter);
     });
 };
-
-if (otwieraczUpgradeButton) {
-    otwieraczUpgradeButton.addEventListener('click', (event) => {
-        counter = otwieraczUpgrade(counter, autoClick);
-    });
-}
-
-if (mietekUpgradeButton) {
-    mietekUpgradeButton.addEventListener('click', (event) => {
-        const result = mietekUpgrade(counter, autoClick);
-
-        counter = result.counter;
-        autoClick = result.autoClick;
-    });
-}
-
-if (sebaUpgradeButton) {
-    sebaUpgradeButton.addEventListener('click', (event) => {
-        const result = sebaUpgrade(counter, autoClick);
-
-        counter = result.counter;
-        autoClick = result.autoClick;
-    });
-}
-
-if (grazynaUpgradeButton) {
-    grazynaUpgradeButton.addEventListener('click', (event) => {
-        const result = grazynaUpgrade(counter, autoClick);
-
-        counter = result.counter;
-        autoClick = result.autoClick;
-    });
-}
-
-if (gangUpgradeButton) {
-    gangUpgradeButton.addEventListener('click', (event) => {
-        const result = gangUpgrade(counter, autoClick);
-
-        counter = result.counter;
-        autoClick = result.autoClick;
-    });
-}
-
-if (monopolowyUpgradeButton) {
-    monopolowyUpgradeButton.addEventListener('click', (event) => {
-        const result = monopolowyUpgrade(counter, autoClick);
-
-        counter = result.counter;
-        autoClick = result.autoClick;
-    });
-}
-
-if (browarUpgradeButton) {
-    browarUpgradeButton.addEventListener('click', (event) => {
-        counter = browarUpgrade(counter, autoClick);
-    });
-}
-
-if (destylarniaUpgradeButton) {
-    destylarniaUpgradeButton.addEventListener('click', (event) => {
-        const result = destylarniaUpgrade(counter, autoClick);
-
-        counter = result.counter;
-        autoClick = result.autoClick;
-    });
-}
 
 window.addEventListener('DOMContentLoaded', (event) => { console.log('DOM fully loaded and parsed'); });
 
