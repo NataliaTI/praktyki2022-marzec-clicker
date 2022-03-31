dbcfc416008981cd88eee2ccba2f2e2f7b0bdc3a
TestkaJakub <testkajakub@gmail.com>
2022-03-31 10:45:16 +0200

    commit do pulla

--- src/Scripts/modules/apiStatus.js
+++ src/Scripts/modules/apiStatus.js
@@ -1,6 +1,5 @@
 // export function status(params) {
 //     if(!localStorage.getItem("points")){
-    
         fetch('../../src/Scripts/test2.json').then(response => response.json())
         .then(data => data.Data.points).then(points => {
             localStorage.setItem("points", points);
