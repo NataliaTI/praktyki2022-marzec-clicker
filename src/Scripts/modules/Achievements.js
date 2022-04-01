c71a95f36370dc31be105ba1052897ce03db14c5
Kacper Firlej <kapicher@gmail.com>
2022-04-01 12:58:37 +0200

    #4

--- src/Scripts/modules/Achievements.js
+++ src/Scripts/modules/Achievements.js
@@ -5,45 +5,54 @@ import { sumOfClicks } from "./statistics";
 const achievementsList = fetch('../../src/Catalog/achievements.json').then(response => response.json());
 console.log(achievementsList)
 
-export function osiagniecia(upgradeLevel, upgradeName) {
-    const achievementLevel = Object.entries(achievementsList);
-    console.log(achievementLevel)
-    for (let index = 0; index < achievementLevel.length; index++) {
-        console.log(upgradeLevel)
-        achievementLevel[index.level] == upgradeLevel;
-        console.log(achievementLevel)
-    }
-};
-
-<<<<<<< HEAD
-
-
-
-<<<<<<< HEAD
-export function osiagniecia(counter) {
-    // console.log('dziala')
-    if (counter == 1) {
-        console.log('dziala')
-    }
-
-    //     if (returnedFormatedValue.counterElement[i] == null || returnedFormatedValue.counterElement[i].text != returnedFormatedValue1.counterElement[i].text) {
-    //         returnedFormatedValue1.counterElement[i] = returnedFormatedValue.counterElement[i];
-    //     }
-    // returnedFormatedValue = returnedFormatedValue1;
-
-=======
 
 
 
 
-=======
->>>>>>> fc6faf2d993fee905513b82984968170a7fe6a5e
+
+
+
+
+
+
+// export function osiagniecia(upgradeLevel, upgradeName) {
+//     const achievementLevel = Object.entries(achievementsList)
+
+// export function osiagniecia(upgradeLevel, upgradeName) {
+
+//     // console.log(achievementsList.achievementLevelOne.level)
+//     // console.log(upgradeLevel)
+
+//     if (achievementsList.achievementLevelOne.level == upgradeLevel)
+//     {
+//         achievementsList.achievementLevelOne.unlocked = true
+//         console.log(upgradeName)
+//         function upgradeName
+//     }
+
+//     if (achievementsList.achievementLevelTwo.level == upgradeLevel)
+//     {
+//         console.log('dziala')
+//     }
+
+//     if (achievementsList.achievementLevelThree.level == upgradeLevel)
+//     {
+//         console.log('dziala')
+//     }
+// };
+
+
+
+
+
+
+
+
       //     if (returnedFormatedValue.counterElement[i] == null || returnedFormatedValue.counterElement[i].text != returnedFormatedValue1.counterElement[i].text) {
         //         returnedFormatedValue1.counterElement[i] = returnedFormatedValue.counterElement[i];
         //     }
         // returnedFormatedValue = returnedFormatedValue1;
     
->>>>>>> 6e48011a4526833a3f92fcd55b69683d7d406832
 
 
 
@@ -55,15 +64,11 @@ export function osiagniecia(counter) {
     //     }
     //     achievement: [{ req: "returnedFormatedValue.counterElement>1", gotten: false, text: "zdobyłeś pierwszy returnedFormatedValue" }];
     // }
-<<<<<<< HEAD
-};
-=======
 
 
 
 
 
->>>>>>> 6e48011a4526833a3f92fcd55b69683d7d406832
 
   // for (i in otwieraczUpgradeLevel) {
         //     if (otwieracz.level[i] == null || otwieracz.level[i].text != otwieracz1.level[i].text) {
