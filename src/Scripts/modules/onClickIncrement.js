02953c3b1b998c61b760c6c1556e56e622865ac4
Kacper Firlej <kapicher@gmail.com>
2022-04-01 09:05:48 +0200

    #4 prima aprilisowe osiągnięcia

--- src/Scripts/modules/onClickIncrement.js
+++ src/Scripts/modules/onClickIncrement.js
@@ -1,5 +1,6 @@
 import { format } from '../Components/format.js';
 import { extraMoneyPerClick } from '../../../Layout/main.js';
+import { osiagniecia } from '../modules/Achievements.js';
 
 const mobileHeaderH2 = document.querySelector('.mobile_header-h2');
 
@@ -8,7 +9,7 @@ const counterElement = document.getElementById("counter");
 
 let returnedFormatedValue = counter;
 
-export function changeCounterElementText(value){
+export function changeCounterElementText(value) {
   returnedFormatedValue = format(value);
 
   counterElement.textContent = returnedFormatedValue + " $";
@@ -21,5 +22,6 @@ export function onClickHandler(counter) {
   counter++;
   changeCounterElementText(counter);
 
-  return counter;  
+  return counter;
 }
+Osiagniecia()
