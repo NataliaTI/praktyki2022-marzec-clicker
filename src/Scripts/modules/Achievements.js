40829391e76161385a2acf4743452b917ae00b39
Kacper Firlej <kapicher@gmail.com>
2022-04-05 09:46:30 +0200

    #4 wspaniale działające wspaniałe osiągnięcia

--- src/Scripts/modules/Achievements.js
+++ src/Scripts/modules/Achievements.js
@@ -4,10 +4,19 @@ export function osiagniecia(upgradeLevel, upgradeName, achivementList) {
     const achievementLevel = Object.keys(achivementList);
     achievementLevel.forEach(achievementKey => {
         if (achivementList[achievementKey].level == upgradeLevel && achivementList[achievementKey].upgradeName == upgradeName) {
+            const szklana = achivementList[achievementKey].upgradeName;
+            const achievement = document.getElementById(achievementKey);
+            const upgradeLevel = achivementList[achievementKey].level;
+            const name = achivementList[achievementKey].locked;
             const header = achivementList[achievementKey].achievementHeader;
             const description = achivementList[achievementKey].achievementDescription;
             const achievementImage = achivementList[achievementKey].iconName;
+            document.getElementById('headline' + szklana + upgradeLevel).innerHTML = header;
+            document.getElementById('specification' + szklana + upgradeLevel).innerHTML = description;
 
+            achievement.querySelector('.achievements-look').classList.add(name);
+            console.log(achievement.querySelector('.achievements-look'))
+            // document.getElementsByClassName('achievements-img').classList.remove('achievements-img');
             showContent(header, description, achievementImage);
         }
     });
