0265a42110070744c438b4923f4a456086536f7a
TestkaJakub <testkajakub@gmail.com>
2022-04-05 09:37:54 +0200

    #7 nowsza wersja apiStatus

--- src/Scripts/modules/apiLogin.js
+++ src/Scripts/modules/apiLogin.js
@@ -1,24 +1,8 @@
 export function login(params) {
     if(!localStorage.getItem("Token")){
-        fetch('http://praktyki-trol-clicker-api.herokuapp.com/login').then(response => response.json())
+        fetch('https://praktyki-trol-clicker-api.herokuapp.com/login').then(response => response.json())
         .then(data => data.Data.Token).then(token => {
             localStorage.setItem("Token", token);
-            //let tok = localStorage.getItem("Token");           
-            //console.log(tok);
         });
-    }else{
-        //console.log("test");    
     }
 }
-//TEST
-/*
-fetch('https://api.github.com/users/NataliaTi/repos').then(
-    resp => resp.json() // this returns a promise
-    ).then(repos => {
-        for (const repo of repos) {
-            document.write(repo.name);
-        }
-    }).catch(ex => {
-        document.write(ex);
-    })
-*/
