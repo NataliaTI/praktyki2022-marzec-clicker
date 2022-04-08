e402c55eea4d2ba2d406382cbe3a7df3ff2ca2ed
Natalia Skorwider <natalia@trol.pl>
2022-04-08 18:14:42 +0200

    dodanie loadera Babel do webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-  entry: './Layout/main.js',
+  mode: 'development',
+  entry: ['babel-polyfill', './Layout/main.js'],
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'Public'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
